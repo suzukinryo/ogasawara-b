c78e1e12a902affe48dab72f31be33cb092eaf95
kondouyutaka <s1742044NS@s.chibakoudai.jp>
2019-06-14 15:57:13 +0900

    c
    d

---
+++ hello(1).php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset='utf-8' />
+    <title>hello</title>
+  </head>
+  <body>
+<?php
+echo 'Hello,' . $_GET['foo']. ',' .$_GET['bar'];
+?>
+  </body>
+</html>
+
